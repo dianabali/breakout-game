551e65b324960a6025c386cb509b16886c4e634a
Diana Bali <dianabali64@gmail.com>
2025-10-22 18:55:06 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const rules = document.getElementById('rules');
+const closeBtn = document.getElementById('close-btn');
+const rulesBtn = document.getElementById('rules-btn');
+const canvas = document.getElementById('canvas');
+
+// Rules and close event listeners
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
